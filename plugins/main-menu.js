f991f42e3a1b7e73f98e99df7b71cdb7ffdb07fc
dev-fedexz <devfedexyz@gmail.com>
2025-11-20 15:44:49 -0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -4,34 +4,34 @@ import fetch from 'node-fetch';
 const getBuffer = async (url) => {
     try {
         const res = await fetch(url);
-        if (res.status!== 200) {
+        if (res.status !== 200) {
             console.error(`Error al descargar la imagen: Código de estado ${res.status}`);
             return null;
-}
+        }
         return await res.buffer();
-} catch (e) {
+    } catch (e) {
         console.error("Error en getBuffer:", e);
         return null;
-}
+    }
 };
 
 let tags = {
-    info: '╭─「 𝖨𝗇ẜᨣ 」─╮',
-    anime: '╭─「 𝖠𝗇ı𝗆ᧉ 」─╮',
-    buscador: '╭─「 Ｓᧉ𝖺ꭇ𝖼𝗁 」─╮',
-    downloader: '╭─「 𝖣𝗈𝗐𝗇𝗅𝗈𝖺𝖽𝗌 」─╮',
-    economy: '╭─「 𝖤𝖼𝗈𝗇𝗈𝗆𝗂𝖺 」─╮',
-    fun: '╭─「 𝖥𝗎𝗇 」─╮',
-    group: '╭─「 𝖦𝗋𝗎𝗉𝗈𝗌 」─╮',
-    ai: '╭─「 𝖨𝗇ƚ𝖾𝗅𝗂𝗀𝖾𝗇𝖼𝗂𝖺 」─╮',
-    game: '╭─「 𝖦𝖺𝗆𝖾𝗌 」─╮',
-    serbot: '╭─「 𝖩𝖺𝖽ı-ᗷ𝗈𝗍𝗌 」─╮',
-    main: '╭─「 𝖯𝗋𝗂𝗇𝖼𝗂𝗉𝖺𝗅 」─╮',
-    nable: '╭─「 𝖮𝗇-𝖮𝖿𝖿 」─╮',
-    nsfw: '╭─「 𝖭𝗌𝖿𝗐 」─╮',
-    owner: '╭─「 𝖮𝗐𝗇𝖾𝗋 」─╮',
-    sticker: '╭─「 𝖲𝗍𝗂𝖼𝗄𝖾𝗋𝗌 」─╮',
-    herramientas: '╭─「 𝖧𝖾𝗋𝗋𝖺𝗆𝗂𝖾𝗇𝗍𝖺𝗌 」─╮'
+    info: '𓂂𓏸  𐅹੭੭   *`𝖨𝗇ẜᨣ`* 🪐 ᦡᦡ',
+    anime: '𓂂𓏸  𐅹੭੭   *`𝖠𝗇ı𝗆ᧉ`* 🥞 ᦡᦡ',
+    buscador: '𓂂𓏸  𐅹੭੭   *`Ｓᧉ𝖺ꭇ𝖼𝗁`* 🌿 ᦡᦡ',
+    downloader: '𓂂𓏸  𐅹੭੭   *`𝖣ᨣ𝗐𝗇𝗅ᨣ𝖺𝖽ᧉꭇ𝗌`* 🍇 ᦡᦡ',
+    economy: '𓂂𓏸  𐅹੭੭   *`𝖾𝖼𝗈𝗆𝗈𝗆𝗂𝖺`* 🌵 ᦡᦡ',
+    fun: '𓂂𓏸  𐅹੭੭   *`𝖥𝗎𝗇`* 🌱 ᦡᦡ',
+    group: '𓂂𓏸  𐅹੭੭   *`Gꭇußꭇ𝗎𝗉ᨣ𝗌`* ☕ ᦡ',
+    ai: '𓂂𓏸  𐅹੭੭   *`𝖨𝗇ƚᧉ𝖨ı𝗀ᧉ𝗇𝖼ı𝖺𝗌`* 🧋 ᦡᦡ',
+    game: '𓂂𓏸  𐅹੭੭   *`Game`* 🥞 ᦡᦡ',
+    serbot: '𓂂𓏸  𐅹੭੭   *`𝖩𝖺𝖽ı-ᗷᨣƚ𝗌`* 🍂 ᦡᦡ',
+    main: '𓂂𓏸  𐅹੭੭   *`𝖯ꭇ𝗂𝗇𝖼𝗂𝗉𝖺𝗅`* ☁️ ᦡᦡ',
+    nable: '𓂂𓏸  𐅹੭੭   *`𝖮𝗇-𝖮ẜẜ`* 🍭 ᦡᦡ',
+    nsfw: '𓂂𓏸  𐅹੭੭   *`𝖭𝗌ẜɯ`* 🪼 ᦡᦡ',
+    owner: '𓂂𓏸  𐅹੭੭   *`Oɯ𝗇ᧉꭇ`* 🧇 ᦡᦡ',
+    sticker: '𓂂𓏸  𐅹੭੭   *`𝖲ƚ𝗂𝖼𝗄ᧉꭇ`* ☘ ᦡᦡ',
+    herramientas: '𓂂𓏸  𐅹੭੭   *`𝖨𝗇ƚᧉꭇ𝗇ᧉƚ`* 🌻 ᦡᦡ'
 };
 
 function clockString(seconds) {
@@ -47,18 +47,33 @@ let handler = async (m, { conn, args}) => {
 
     let nombre = await conn.getName(m.sender);
     let user = global.db.data.users[m.sender];
-    let premium = user?.premium? 'sɪ́': 'ɴᴏ';
+    let premium = user?.premium ? 'sɪ́' : 'ɴᴏ';
     let totalreg = Object.keys(global.db.data.users).length;
     let groupsCount = Object.values(conn.chats).filter(v => v.id.endsWith('@g.us')).length;
     let uptime = clockString(process.uptime());
+    
+    const defaultTag = 'Otros';
 
     for (let plugin of Object.values(global.plugins)) {
-        if (!plugin.help ||!plugin.tags) continue;
-        for (let tag of plugin.tags) {
-            if (!categories[tag]) categories[tag] = [];
-            categories[tag].push(...plugin.help.map(cmd => `${cmd}`));
-}
-}
+        if (plugin.help && plugin.tags) {
+            for (let tag of plugin.tags) {
+                if (!categories[tag]) categories[tag] = [];
+                categories[tag].push(...plugin.help.map(cmd => `${cmd}`));
+            }
+        } else if (plugin.command) {
+            let cmdArray = Array.isArray(plugin.command) ? plugin.command : [plugin.command];
+            
+            let displayCmd = cmdArray[0]; 
+
+            let tagKey = plugin.tags && plugin.tags.length > 0 ? plugin.tags[0] : defaultTag;
+            
+            if (!categories[tagKey]) categories[tagKey] = [];
+            
+            if (!categories[tagKey].includes(displayCmd)) {
+                 categories[tagKey].push(displayCmd);
+            }
+        }
+    }
 
     let infoUser = `
 ❐ 𝖧𝗈𝗅𝖺, 𝖲𝗈𝗒 *_𝖲𝗁𝖺𝖽𝗈𝗐 - 𝖡𝗈𝗍_* 🌱
@@ -79,7 +94,7 @@ let handler = async (m, { conn, args}) => {
     for (let [tag, cmds] of Object.entries(categories)) {
         let tagName = tags[tag] || `╭─「 ${tag.toUpperCase()} 」─╮`;
         menuText += `${tagName}\n${cmds.map(cmd => `➩ ${cmd}`).join('\n')}\n\n`;
-}
+    }
 
     await conn.sendMessage(m.chat, {
         text: menuText,
@@ -91,16 +106,16 @@ let handler = async (m, { conn, args}) => {
                 sourceUrl: 'https://github.com/Shadows-club',
                 mediaType: 1,
                 renderLargerThumbnail: true
-},
+            },
             mentionedJid: [m.sender, userId],
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
                 newsletterJid: global.canalIdM[0],
                 newsletterName: '𝖲𝗁𝖺𝖽𝗈𝗐 - 𝖡𝗈𝗍',
                 serverMessageId: -1
-}
-}
-}, { quoted: m});
+            }
+        }
+    }, { quoted: m});
 };
 
 handler.help = ['menu'];
