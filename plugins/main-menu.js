dc927bfbd82926ded3276f481551487dfbe98bc2
dev-fedexz <devfedexyz@gmail.com>
2025-11-20 14:10:36 -0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -17,39 +17,54 @@ const getBuffer = async (url) => {
 
 let tags = { info: '𓂂𓏸 𐅹੭੭ *`𝖨𝗇ẜᨣ`* 🪐 ᦡᦡ', anime: '𓂂𓏸 𐅹੭੭ *`𝖠𝗇ı𝗆ᧉ`* 🥞 ᦡᦡ', buscador: '𓂂𓏸 𐅹੭੭ *`Ｓᧉ𝖺ꭇ𝖼𝗁`* 🌿 ᦡᦡ', downloader: '𓂂𓏸 𐅹੭੭ *`𝖣ᨣ𝗐𝗇𝗅ᨣ𝖺𝖽ᧉꭇ𝗌`* 🍇 ᦡᦡ', economy: '𓂂𓏸 𐅹੭੭ *`𝖾𝖼𝗈𝗆𝗈𝗆𝗂𝖺`* 🌵 ᦡᦡ', fun: '𓂂𓏸 𐅹੭੭ *`𝖥𝗎𝗇`* 🌱 ᦡᦡ', group: '𓂂𓏸 𐅹੭੭ *`Gꭇußꭇ𝗎𝗉ᨣ𝗌`* ☕ ᦡ', ai: '𓂂𓏸 𐅹੭੭ *`𝖨𝗇ƚᧉ𝖨ı𝗀ᧉ𝗇𝖼ı𝖺𝗌`* 🧋 ᦡᦡ', game: '𓂂𓏸 𐅹੭੭ *`Game`* 🥞 ᦡᦡ', serbot: '𓂂𓏸 𐅹੭੭ *`𝖩𝖺𝖽ı-ᗷᨣƚ𝗌`* 🍂 ᦡᦡ', main: '𓂂𓏸 𐅹੭੭ *`𝖯ꭇ𝗂𝗇𝖼𝗂𝗉𝖺𝗅`* ☁️ ᦡᦡ', nable: '𓂂𓏸 𐅹੭੭ *`𝖮𝗇-𝖮ẜẜ`* 🍭 ᦡᦡ', nsfw: '𓂂𓏸 𐅹੭੭ *`𝖭𝗌ẜɯ`* 🪼 ᦡᦡ', owner: '𓂂𓏸 𐅹੭੭ *`Oɯ𝗇ᧉꭇ`* 🧇 ᦡᦡ', sticker: '𓂂𓏸 𐅹੭੭ *`𝖲ƚ𝗂𝖼𝗄ᧉꭇ`* ☘ ᦡᦡ', herramientas: '𓂂𓏸 𐅹੭੭ *`𝖨𝗇ƚᧉꭇ𝗇ᧉƚ`* 🌻 ᦡᦡ' };
 
+function clockString(seconds) {
+    let h = Math.floor(seconds / 3600);
+    let m = Math.floor(seconds % 3600 / 60);
+    let s = Math.floor(seconds % 60);
+    return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':');
+}
+
 let handler = async (m, { conn, args }) => {
     let userId = m.mentionedJid?.[0] || m.sender
     let categories = {}
+    
+    let nombre = await conn.getName(m.sender);
+    let user = global.db.data.users[m.sender];
+    let premium = user?.premium ? 'sɪ́' : 'ɴᴏ'; 
+    let totalreg = Object.keys(global.db.data.users).length;
+    let groupsCount = Object.values(conn.chats).filter(v => v.id.endsWith('@g.us')).length;
+    let uptime = clockString(process.uptime());
     
     for (let plugin of Object.values(global.plugins)) {
         if (!plugin.help || !plugin.tags) continue
         for (let tag of plugin.tags) {
             if (!categories[tag]) categories[tag] = []
-            categories[tag].push(...plugin.help.map(cmd => `#${cmd}`))
+            categories[tag].push(...plugin.help.map(cmd => `${cmd}`))
         }
     }
 
-    let totalreg = Object.keys(global.db.data.users).length
-    let totalCommands = Object.values(global.plugins).filter((v) => v.help && v.tags).length
-    
-    let menuText = `•——————•°•✿•°•——————•
-╰┈➤ MαყBσƚ ⌇°•
-⊱┊ ᴴᵉᶜʰᵒ ᵖᵒʳ ${global.etiqueta}
-●～●～●～●～●～●～●～●～
+    let infoUser = `
+❐ 𝖧𝗈𝗅𝖺, 𝖲𝗈𝗒 *_𝖲𝗁𝖺𝖽𝗈𝗐 - 𝖡𝗈𝗍_* 🌱 
 
-➮ 𝐇𝐨𝐫𝐚: °❀ *${global.hora}*
-➮ 𝐅𝐞𝐜𝐡𝐚: °❀ *${global.fecha}*
-➮ 𝐓𝐢𝐩𝐨: °❀ *${(conn.user.jid == global.conn.user.jid ? 'Principal' : 'Sub-Bot')}*
-➮ 𝐔𝐬𝐮𝐚𝐫𝐢𝐨𝐬: °❀ *${totalreg.toLocaleString()}*
-ׂ╰┈➤ *${totalCommands}* ℂ𝕠𝕞𝕒𝕟𝕕𝕠𝕤 𝕕𝕚𝕤𝕡𝕠𝕟𝕚𝕓𝕝𝕖𝕤.\n`.trim()
+╰┈□ 𝖨𝖭𝖥𝖮-𝖴𝖲𝖤𝖱
+❐ _𝖴𝗌𝗎𝖺𝗋𝗂𝗈:_ ${nombre}
+❐ _𝖯𝗋𝖾𝗆𝗂𝗎𝗆:_ ${premium}
+❐ _𝖱𝖾𝗀𝗂𝗌𝗍𝗋𝖺𝖽𝗈𝗌 𝗍𝗈𝗍𝖺𝗅𝖾𝗌:_ ${totalreg}
+
+╰┈□ 𝖨𝖭𝖥𝖮-𝖡𝖤𝖳
+❐ _𝖳𝗂𝖾𝗆𝗉𝗈 𝖺𝖼𝗍𝗂𝗏𝗈:_ ${uptime}
+❐ _𝖦𝗋𝗎𝗉𝗈𝗌 𝖼𝗈𝗇𝗍𝖺𝖽𝗈𝗌:_ ${groupsCount}
+❐ _𝖥𝖾𝖼𝗁𝖺 𝖺𝖼𝗍𝗎𝖺𝗅:_ [${new Date().toLocaleString('es-ES')}]
+`.trim();
+
+    let menuText = infoUser + '\n'
 
     for (let [tag, cmds] of Object.entries(categories)) {
         let tagName = tags[tag] || tag 
         menuText += `
-ೃ‧₊› ${tagName} ：
-${cmds.map(cmd => `╰┈➤ ${cmd}`).join('\n')}
+${tagName} ：
+${cmds.map(cmd => `➩ ${cmd}`).join('\n')}
 
-↶*ೃ✧˚. ❃ ↷ ˊ-↶*ೃ✧˚. ❃ ↷ ˊ-
 `
     }
 
@@ -58,9 +73,9 @@ ${cmds.map(cmd => `╰┈➤ ${cmd}`).join('\n')}
         contextInfo: {
             externalAdReply: {
                 title: global.canalNombreM[0],
-                body: '⊱┊ MαყBσƚ ᵇʸ ˢᵒʸᵐᵃყᶜᵒˡ ❦',
+                body: '𝑺𝒉𝒂𝒅𝒐𝒘`𝑺 - 𝑩𝒐𝒕',
                 thumbnailUrl: 'https://i.postimg.cc/SQTP9YCm/4-sin-titulo-20251120074041.jpg',
-                sourceUrl: 'https://mayapi.ooguy.com',
+                sourceUrl: 'hhttps://github.com/Shadows-club',
                 mediaType: 1,
                 renderLargerThumbnail: true
             },
