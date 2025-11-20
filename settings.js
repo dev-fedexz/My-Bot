37c56f977d52a3c8c1edc1ba8e79eec815ec186a
dev-fedexz <devfedexyz@gmail.com>
2025-11-20 12:03:05 -0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,69 @@
+import { watchFile, unwatchFile } from "fs"
+import chalk from "chalk"
+import { fileURLToPath } from "url"
+import fs from "fs"
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
+//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
+global.botNumber = "" //Ejemplo: 573218138672
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.owner = ["51921826291"]
+global.suittag = [] 
+global.prems = []
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.libreria = "@soymaycol/baileys"
+global.vs = "Latest"
+global.sessions = "MayBot/Principal"
+global.jadi = "MayBot/Subbots"
+global.mayJadibts = true
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.botname = "MαყBσƚ ©"
+global.textbot = "MαყBσƚ © • ცყ ʂơყɱąყƈơƖ"
+global.dev = "ʂơყɱąყƈơƖ"
+global.author = "ʂơყɱąყƈơƖ <3"
+global.etiqueta = "ˢᵒʸᵐᵃʸᶜᵒˡ"
+global.currency = "𝐃𝐨𝐥𝐚𝐫𝐞𝐬"
+global.banner = "https://files.catbox.moe/x8xyh8.jpeg"
+global.icono = "https://files.catbox.moe/s4aorc.jpg"
+global.catalogo = fs.readFileSync('./lib/catalogo.jpg')
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.group = "https://chat.whatsapp.com/JNQMGcG9jl00jYBM1iV8Jn"
+global.community = "https://chat.whatsapp.com/KqkJwla1aq1LgaPiuFFtEY"
+global.channel = "https://whatsapp.com/channel/0029VbBBNfH4Y9ltpS4C8w3c"
+global.github = "https://github.com/SoySapo6/MayBot"
+global.gmail = "soymaycol.cn@gmail.com"
+global.ch = {
+ch1: "120363406779062566@newsletter"
+}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.APIs = {
+xyro: { url: "https://api.xyro.site", key: null },
+yupra: { url: "https://api.yupra.my.id", key: null },
+vreden: { url: "https://api.vreden.web.id", key: null },
+delirius: { url: "https://api.delirius.store", key: null },
+zenzxz: { url: "https://api.zenzxz.my.id", key: null },
+siputzx: { url: "https://api.siputzx.my.id", key: null },
+adonix: { url: "https://api-adonix.ultraplus.click", key: 'Destroy-xyz' },
+mayapi: { url: "https://mayapi.ooguy.com", key: 'soymaycol' }
+}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+unwatchFile(file)
+console.log(chalk.redBright("Update 'settings.js'"))
+import(`${file}?update=${Date.now()}`)
+})
