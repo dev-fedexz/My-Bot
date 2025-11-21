11588af4a493588a485ac21757fe677600efe612
dev-fedexz <devfedexyz@gmail.com>
2025-11-21 11:13:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 
 let { say } = cfonts
 console.log(chalk.magentaBright('\n🌱 Iniciando...'))
-say('Shadow - Bot, {
+say('Shadow - Bot', {
 font: 'BLOCK',
 align: 'center',
 gradient: ['grey', 'white']
