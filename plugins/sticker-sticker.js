3694a7310c6f34a303a9a8ccbbea439e68d48067
dev-fedexz <devfedexyz@gmail.com>
2025-11-20 21:19:38 -0300

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -108,4 +108,4 @@ function tryAutoReduce(buffer) {
 
 function isUrl(text) {
   return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png|webp)/, 'gi'))
-}
+          }
